92edbf38c9b10f6b64333a4c3282a9bff91ce6ab
SafarGo <159808426+SafarGo@users.noreply.github.com>
2025-05-26 16:55:20 +0300

    UI_update_commit

--- Microb_Battle/Assets/Scripts/GameManager.cs
+++ Microb_Battle/Assets/Scripts/GameManager.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class GameManager : MonoBehaviour
 {
-    public static float Glukoza = 100;
+    public static float Glukoza = 10;
     public static List<Vector3> points = new List<Vector3>();
     public static int i = 0;
     public static List<GameObject> towers = new List<GameObject>();
