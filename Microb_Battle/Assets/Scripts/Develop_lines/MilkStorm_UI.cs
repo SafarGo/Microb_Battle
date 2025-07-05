55a9129e9389c83426f683c52d5be5c45c02c45a
SafarGo <159808426+SafarGo@users.noreply.github.com>
2025-07-05 21:02:37 +0300

    Milkstrom_almost_ready

--- Microb_Battle/Assets/Scripts/Develop_lines/MilkStorm_UI.cs
+++ Microb_Battle/Assets/Scripts/Develop_lines/MilkStorm_UI.cs
@@ -5,29 +5,35 @@ using UnityEngine;
 public class MilkStorm_UI : MonoBehaviour
 {
     public ParticleSystem sys;
+    bool activate_storm = false;
 
     private void Start()
     {
-        sys.GetComponent<ParticleSystem>();
     }
     public void OnClickButton()
     {
-        if(GameManager.isMilkStorm1 == false)
+        activate_storm = false;
+        if (!GameManager.isMilkStorm1 && !GameManager.isMilkStorm2 && !GameManager.isMilkStorm3)
         {
             return;
         }
         else if(GameManager.isMilkStorm1)
         {
             MilkStorm_line.instance.Storm1();
+            activate_storm = true;
+
         }
         else if(GameManager.isMilkStorm2)
         {
             MilkStorm_line.instance.Storm2();
+            activate_storm = true;
         }
         else if (GameManager.isMilkStorm3)
         {
             MilkStorm_line.instance.Storm3();
+            activate_storm = true;
         }
+        if(activate_storm)
         sys.Play();
     }
 }
--- Microb_Battle/Assets/Scripts/Develop_lines/MilkStorm_line.cs
+++ Microb_Battle/Assets/Scripts/Develop_lines/MilkStorm_line.cs
@@ -1,24 +1,29 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MilkStorm_line : MonoBehaviour
 {
     public GameObject storm;
     private ParticleSystem sys;
     public static MilkStorm_line instance;
+    public List<GameObject> buttons = new List<GameObject>();
 
     private void Start()
     {
         sys = storm.GetComponent<ParticleSystem>();
         instance = this;
-
     }
     public void Storm1()
     {
         var collision = sys.collision;
         collision.collidesWith = (1 << LayerMask.NameToLayer("Units"));
-        GameManager.isMilkStorm1 = true;
+        GameManager.isMilkStorm2 = true;
+        GameManager.isMilkStorm1 = false;
+        buttons[0].GetComponent<Button>().interactable = false;
+        buttons[1].GetComponent<Button>().interactable = true;
+        buttons[2].GetComponent<Button>().interactable = false;
     }
 
     public void Storm2()
@@ -26,9 +31,14 @@ public class MilkStorm_line : MonoBehaviour
         var collision = sys.collision;
         collision.collidesWith = (1 << LayerMask.NameToLayer("Units")) | (1 << LayerMask.NameToLayer("Attack"));
         var _speed = sys.main.startSpeed;
+        var emission = sys.emission;
+        emission.rateOverTime = 100;
         _speed = 20f;
-        GameManager.isMilkStorm2 = true;
-        GameManager.isMilkStorm1 = false;
+        GameManager.isMilkStorm3 = true;
+        GameManager.isMilkStorm2 = false;
+        buttons[0].GetComponent<Button>().interactable = false;
+        buttons[1].GetComponent<Button>().interactable = false;
+        buttons[2].GetComponent<Button>().interactable = true;
     }
 
     public void Storm3()
@@ -37,8 +47,11 @@ public class MilkStorm_line : MonoBehaviour
         collision.collidesWith = (1 << LayerMask.NameToLayer("Units")) | (1 << LayerMask.NameToLayer("Attack")) | (1 << LayerMask.NameToLayer("Nodes"));
         var _speed = sys.main.startSpeed;
         _speed = 30f ;
-        GameManager.isMilkStorm3 = true;
-        GameManager.isMilkStorm2 = false;
+        var emission = sys.emission;
+        emission.rateOverTime = 200;
+        buttons[0].GetComponent<Button>().interactable = false;
+        buttons[1].GetComponent<Button>().interactable = false;
+        buttons[2].GetComponent<Button>().interactable = false;
     }
 
 }
