bdaf13dbf56b7ed0b71f626bc3e255f8284c6770
HardGrin <greenelekt@gmail.com>
2025-07-08 18:05:00 +0300

    18_03_Sasha_Commit

---
+++ Microb_Battle/Assets/Scripts/Network/Connection.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+using Photon;
+using Photon.Realtime;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class Connection : MonoBehaviourPunCallbacks
+{
+    private void Start()
+    {
+        PhotonNetwork.ConnectUsingSettings();
+    }
+
+    public override void OnConnectedToMaster()
+    {
+        Debug.Log("Connected to server");
+        SceneManager.LoadScene("MainMenu");
+    }
+}
---
+++ Microb_Battle/Assets/Scripts/Network/MenuNetworkManager.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+using Photon;
+using Photon.Realtime;
+using Photon.Pun;
+using UnityEngine.SceneManagement;
+
+public class MenuNetworkManager : MonoBehaviourPunCallbacks
+{
+    public void CreateRoom()
+    {
+        RoomOptions roomOptions = new RoomOptions();
+        roomOptions.MaxPlayers = 2;
+        PhotonNetwork.CreateRoom("Room", roomOptions, TypedLobby.Default);
+    }
+
+    public void JoinRandomRoom()
+    {
+        PhotonNetwork.JoinRandomRoom();
+    }
+
+    public override void OnJoinedRoom()
+    {
+        SceneManager.LoadScene("SampleScene");
+        Debug.Log("joined to game");
+    }
+}
