dc95ccc8917590f79438578dab4e06150c166f44
HardGrin <greenelekt@gmail.com>
2025-07-08 20:39:49 +0300

    test_3_commit

--- Microb_Battle/Assets/Scripts/Network/isMine.cs
+++ Microb_Battle/Assets/Scripts/Network/isMine.cs
@@ -9,12 +9,14 @@ public class isMine : MonoBehaviourPunCallbacks
 {
     [SerializeField] private PhotonView _photonView;
     [SerializeField] private CameraController controller;
+    [SerializeField] private GameObject _camera;
 
     void Start()
     {
         _photonView = GetComponent<PhotonView>();
         if (!_photonView.IsMine)
         {
+            _camera.SetActive(false);
             controller.enabled = false;
         }
     }
