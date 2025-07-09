97a4e7bae62fa6d77ec8b31fcda6246371361e2a
HardGrin <greenelekt@gmail.com>
2025-07-09 03:30:13 +0300

    SashaCommit_OtherSidesSync_Complete

--- Microb_Battle/Assets/Scripts/Network/PlayerSideController.cs
+++ Microb_Battle/Assets/Scripts/Network/PlayerSideController.cs
@@ -1,22 +1,85 @@
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
 using Photon.Pun;
-using Unity.VisualScripting;
+using System;
+using System.Linq;
+using UnityEngine;
 
 public class PlayerSideController : MonoBehaviour
 {
     public bool isAtaker;
     public static PlayerSideController instance;
+    [SerializeField] private PhotonView _photonView;
     private void Start()
     {
-        if(PhotonNetwork.CountOfPlayers == 1)
+        if (!_photonView.IsMine) return;
+            Debug.Log(PhotonNetwork.CountOfPlayers);
+
+
+        if (PhotonNetwork.CountOfPlayers == 1)
         {
-            isAtaker = false;
+                isAtaker = false;
         }
         else
         {
-            isAtaker = true;
+                isAtaker = true;
         }
+
+        if(isAtaker == true)
+        {
+            if(_photonView.IsMine)
+                DisableScriptsByNames("BuildWalls");
+        }
+    }
+
+    /// <summary>
+    /// Отключает компоненты по списку названий классов
+    /// </summary>
+    public void DisableScriptsByNames(params string[] scriptNames)
+    {
+        int totalDisabled = 0;
+
+        foreach (string name in scriptNames)
+        {
+            Type type = FindTypeByName(name);
+
+            if (type == null)
+            {
+                Debug.LogWarning($"Класс '{name}' не найден.");
+                continue;
+            }
+
+            UnityEngine.Object[] foundObjects = FindObjectsOfType(type, true);
+
+            foreach (var obj in foundObjects)
+            {
+                if (obj is Behaviour behaviour)
+                {
+                    behaviour.enabled = false;
+                    totalDisabled++;
+                }
+            }
+        }
+
+        Debug.Log($"Отключено {totalDisabled} компонент(ов).");
+    }
+
+    /// <summary>
+    /// Ищет тип по имени в загруженных сборках
+    /// </summary>
+    private Type FindTypeByName(string className)
+    {
+        // Пробуем найти с полным namespace
+        Type type = Type.GetType(className);
+        if (type != null)
+            return type;
+
+        // Поиск по всем сборкам
+        foreach (var asm in AppDomain.CurrentDomain.GetAssemblies())
+        {
+            type = asm.GetTypes().FirstOrDefault(t => t.Name == className);
+            if (type != null)
+                return type;
+        }
+
+        return null;
     }
 }
